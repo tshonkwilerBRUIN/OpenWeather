d877cdbec88dcb2eaf3e27e902cdfb064a657365
tshonkwilerBRUIN <140534169+tshonkwilerBRUIN@users.noreply.github.com>
2023-08-09 16:35:42 +0000

    Readded tuples for State checks in case file issue occurs

--- main.py
+++ main.py
@@ -137,19 +137,40 @@ def checkUSAState(state_name):
 
 def USAStateNames():
     '''Returns tuple of USA state names'''
-    with open('state_names.txt') as f:
-        reader = csv.reader(f, delimiter=',')
-        state_names = list(reader)
-
-    return tuple(state_names[0])
+    try:
+        with open('state_names.txt') as f:
+            reader = csv.reader(f, delimiter=',')
+            state_names = list(reader)
+    except Exception:
+        state_names = ('Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 
+                       'Connecticut','Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
+                       'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+                       'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+                       'Mississippi', 'Missouri', 'Montana', 'Nebraska','Nevada',
+                       'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 
+                       'North Carolina','North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 
+                       'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
+                       'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+                       'West Virginia', 'Wisconsin', 'Wyoming')
+        return state_names
+    else:
+        return tuple(state_names[0])
 
 def USAStateAbbreviations():
     '''Returns tupe of USA state abbreviations'''
-    with open('state_abbreviations.txt') as f:
-        reader = csv.reader(f, delimiter=',')
+    try:     
+        with open('state_abbreviations.txt') as f:
+         reader = csv.reader(f, delimiter=',')
         state_abbreviations = list(reader)
-        
-    return tuple(state_abbreviations[0])
+    except Exception:
+        state_abbreviations = ('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 
+                               'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+                               'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+                               'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+                               'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY')
+        return state_abbreviations
+    else:
+        return tuple(state_abbreviations[0])
 
 def main():
     '''Main function'''
